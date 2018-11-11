4735596d6db6c64e7606fb02becabc370de32e03
DanKing1903 <drking1@sheffield.ac.uk>
2018-11-11 15:51:51 +0000

    Refactored make_datasets.py into function

--- src/utils/make_datasets.py
+++ src/utils/make_datasets.py
@@ -3,25 +3,40 @@ import pickle
 from collections import namedtuple
 from sklearn.model_selection import train_test_split
 from pathlib import Path
-seed = 42
+from src.utils.preprocessing import preprocess_text
+import random
+from nltk import word_tokenize, pos_tag
+import argparse
 
-all_review_paths = []
+def make_data_pickle(toy_dataset=False):
+    seed = 42
+    random.seed(seed)
+    paths = []
 
-for f in ['pos', 'neg']:
-    all_review_paths.extend(Path('.').glob('aclImdb/*/{}/*.txt'.format(f)))
+    for f in ['pos', 'neg']:
+        paths.extend(Path('.').glob('aclImdb/*/{}/*.txt'.format(f)))
 
-record = namedtuple('record', ['X', 'y', 'path'])
-all_records = []
+    record = namedtuple('record', ['doc', 'sentiment'])
+    all_records = []
 
-for path in all_review_paths:
-    review = path.read_text()
-    sentiment = 1 if 'pos' in str(path) else 0
-    all_records.append(record(review, sentiment,path))
+    if toy_dataset:
+        paths = random.sample(paths, 1000)
 
-df = pd.DataFrame.from_records(all_records, columns=record._fields)
+    for path in paths:
+        raw = path.read_text()
+        doc = preprocess_text(raw)
+        sentiment = 1 if 'pos' in str(path) else 0
+        all_records.append(record(doc, sentiment))
 
-train, test = train_test_split(df, stratify=df.y, test_size=0.2, random_state=42)
-train, val = train_test_split(train, stratify=train.y, test_size=1/8, random_state=42)
+    data = pd.DataFrame.from_records(all_records, columns=record._fields)
 
-with Path('./data/imdb.pickle').resolve().open(mode='wb') as f:
-    pickle.dump((train, val, test),f)
+    with Path('./data/imdb.pickle').resolve().open(mode='wb') as f:
+        pickle.dump(data,f)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Create, preprocess and serialize data ")
+    parser.add_argument('-t', '--toy_dataset', action="store_true", help="Use reduced dataset size for development and debugging")
+    args = parser.parse_args()
+
+    make_data_pickle(args.toy_dataset)
